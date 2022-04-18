7b9a516b1aa619ac92f0e1a89ca24c3dac0d4db5
Angel <a20190192@pucp.edu.pe>
2022-04-18 10:43:29 -0500

    primer commit

---
+++ src/main/java/pe/edu/pucp/lab4_fp_grupo2/Lab4FpGrupo2Application.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.lab4_fp_grupo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4FpGrupo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4FpGrupo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/pucp/lab4_fp_grupo2/Lab4FpGrupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.lab4_fp_grupo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab4FpGrupo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
