efb45a8306669fa6abd2eb4dfa27cc30e93404cc
Alessandra Morales <azmoralesm@pucp.edu.pe>
2022-04-18 11:48:07 -0500

    Querys completos

--- src/main/java/pe/edu/pucp/lab4_fp_grupo2/repository/DepartmentsRepository.java
+++ src/main/java/pe/edu/pucp/lab4_fp_grupo2/repository/DepartmentsRepository.java
@@ -1,5 +1,8 @@
 package pe.edu.pucp.lab4_fp_grupo2.repository;
 
+import org.springframework.data.jpa.repository.Query;
+import pe.edu.pucp.lab4_fp_grupo2.dto.EmpleadosDepartamentoDto;
+import pe.edu.pucp.lab4_fp_grupo2.dto.SueldoDepartamentoDto;
 import pe.edu.pucp.lab4_fp_grupo2.entity.Departments;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,5 +14,17 @@ import java.util.List;
 @Repository
 public interface DepartmentsRepository extends JpaRepository<Departments,Integer> {
 
+    @Query(value = "select  d.department_id as iddep, d.department_name as namedep, round(avg(e.salary)) as promedio\n" +
+            "from departments d\n" +
+            "join employees e on (d.department_id= e.department_id)\n" +
+            "group by d.department_id;",nativeQuery = true)
+    List<SueldoDepartamentoDto> listaSueldoDepartamento();
+
+    @Query(value = "select e.employee_id as employeeid, e.first_name as nombre, e.last_name as apellido, j.job_title as cargo, e.salary as sueldo\n" +
+            "from employees e\n" +
+            "join jobs j on (e.job_id=j.job_id) \n" +
+            "where e.department_id = ?1 \n" +
+            "order by e.salary desc;" ,nativeQuery = true)
+    List<EmpleadosDepartamentoDto> listaEmpleadoDepartmento (int iddep);
 
 }
--- src/main/java/pe/edu/pucp/lab4_fp_grupo2/repository/EmployeesRepository.java
+++ src/main/java/pe/edu/pucp/lab4_fp_grupo2/repository/EmployeesRepository.java
@@ -17,8 +17,11 @@ public interface EmployeesRepository extends JpaRepository<Employees,Integer> {
             "or j.job_title like %?1% or l.city like %?1%;")
     List<Employees> listarEmpleadosNombreApellidoDepartamentoPuestoCiudad(String searchTxt);
 
-    @Query(nativeQuery = true, value="")
-    List<Employees> listarEmpleadosMayorSalario(String searchTxt);
+    @Query(nativeQuery = true, value="select e.first_name, e.last_name, jh.start_date as fechainicio, jh.end_date as fechafin, j.job_title as puesto,e.salary from employees e \n" +
+            "inner join job_history jh on (jh.employee_id=e.employee_id)\n" +
+            "inner join jobs j on (e.job_id=j.job_id) \n" +
+            "order by e.salary desc;")
+    List<Employees> listarEmpleadosMayorSalario();
 
 
 }
